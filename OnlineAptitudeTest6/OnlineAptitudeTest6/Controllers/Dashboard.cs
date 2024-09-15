7b864e58d242d8f230e53a6b8d708afdcbe00e68
MuhammadIbrahimbaig <138751972+MuhammadIbrahimbaig@users.noreply.github.com>
2024-09-15 18:25:47 +0500

    question

---
+++ OnlineAptitudeTest6/OnlineAptitudeTest6/Controllers/Dashboard.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineAptitudeTest6.Models;
+namespace OnlineAptitudeTest6.Controllers
+{
+    public class Dashboard : Controller
+    {
+        //Private Read Context File
+        private readonly AptitudeTestContext db;
+        private readonly IHttpContextAccessor Contx;
+
+
+        private readonly ILogger<Dashboard> _logger;
+        public Dashboard(ILogger<Dashboard> logger, AptitudeTestContext db, IHttpContextAccessor contx)
+        {
+            _logger = logger;
+            this.db = db;
+            this.Contx = contx;
+        }
+
+        //[HttpGet]
+        //public IActionResult QuestionAdd()
+        //{
+
+
+        //    QuestionAddViewModel add = new QuestionAddViewModel
+        //    {
+        //        Testlist = db.Tests.ToList(),
+        //        Question = new Question()
+        //    };
+        //    db.Questions.Add(QuestionAddViewModel);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Signin");
+        //    //return View(add);
+        //}
+        [HttpGet]
+        public IActionResult QuestionAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult QuestionAdd(string Questionadd)
+        {
+            if (!string.IsNullOrEmpty(Questionadd))
+            {
+                var add = new Question { QuestionText = Questionadd , OptionA = Questionadd, OptionB = Questionadd, OptionC = Questionadd, OptionD = Questionadd, CorrectOption = Questionadd, Marks = CoQuestionadd};
+                db.Questions.Add(add);
+                db.SaveChanges();
+            }
+            return RedirectToAction("QuestionAdd");
+        }
+
+
+
+        public IActionResult QuestionShow()
+        {
+            return View();
+        }
+    }
+}
---
+++ OnlineAptitudeTest6/OnlineAptitudeTest6/Models/QuestionAddViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineAptitudeTest6.Models
+{
+    public class QuestionAddViewModel
+    {
+        public IEnumerable<Test> Testlist { get; set; }
+        public Question Question { get; set; }
+    }
+}
