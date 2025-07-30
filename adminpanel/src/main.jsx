0cb3797f0a3bfc3995afb350ed1339b2ba68566b
Ibrahimbaig-collab <ibrahim.pbhub023@gmail.com>
2025-07-30 19:49:06 +0500

    Add the Fontawesome import

--- adminpanel/src/main.jsx
+++ adminpanel/src/main.jsx
@@ -2,11 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';  
-import 'bootstrap/dist/js/bootstrap.bundle.js'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../src/assets/css/sb-admin-2.css';
-// import '../src/assets/css/sb-admin-2.min.css';
 import '../src/assets/js/sb-admin-2.js'
 import '../src/assets/js/sb-admin-2.min.js'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+ 
+
+
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
   
